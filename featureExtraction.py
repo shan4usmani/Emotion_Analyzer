5607936221bbbe3752da1b8c05e23778abeea9d3
shan4usmani <82283635+shan4usmani@users.noreply.github.com>
2021-04-11 04:16:53 +0500

    Add files via upload

---
+++ featureExtraction.py
@@ -0,0 +1,73 @@
+import librosa
+import numpy as np
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+
+'''
+    function: extract_features
+    input: path to mp3 files
+    output: array containing features extracted
+
+    this file takes in the path to the audio file and extracts the relevant features 
+    from that file for audio processing.
+'''
+
+
+def extract_features(data):
+    # ZCR
+    result = np.array([])
+    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+    result=np.hstack((result, zcr)) # stacking horizontally
+
+    # Chroma_stft
+    stft = np.abs(librosa.stft(data))
+    chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, chroma_stft)) # stacking horizontally
+
+    # MFCC
+    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mfcc)) # stacking horizontally
+
+    # Root Mean Square Value
+    rms = np.mean(librosa.feature.rms(y=data).T, axis=0)
+    result = np.hstack((result, rms)) # stacking horizontally
+
+    # MelSpectogram
+    mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+    result = np.hstack((result, mel)) # stacking horizontally
+            
+    return result
+
+def get_features(path):
+    data, sample_rate = librosa.load(path, duration=2.5, offset=0.6)
+    
+    # without augmentation
+    res1 = extract_features(data)
+    result = np.array(res1)
+    
+    # data with noise
+    noise_data = noise(data)
+    res2 = extract_features(noise_data)
+    result = np.vstack((result, res2)) # stacking vertically
+    
+    # data with stretching and pitching
+    new_data = stretch(data)
+    data_stretch_pitch = pitch(new_data, sample_rate)
+    res3 = extract_features(data_stretch_pitch)
+    result = np.vstack((result, res3)) # stacking vertically
+    
+    return result
+
+file_path = filedialog.askopenfilename()
+X, Y = [], []
+# for path, emotion in zip(data_path.Path, data_path.Emotions):
+feature = get_features(file_path)
+for ele in feature:
+    X.append(ele)
+    # appending emotion 3 times as we have made 3 augmentation techniques on each audio file.
+    # Y.append(emotion)
+
+print(X)
+
+
